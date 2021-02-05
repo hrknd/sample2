0bb141557ed0eb8d23110e463f28e02bf40140a1
KONDO, Hiroyasu <hrk@homi.jp>
2021-02-05 16:55:04 +0900

    production build

--- public/4.js
+++ public/4.js
@@ -1,42 +1,9 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[4],{
 
-/***/ "./resources/js/Pages/Bookmark/List/index.tsx":
-/*!****************************************************!*\
-  !*** ./resources/js/Pages/Bookmark/List/index.tsx ***!
-  \****************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
-var layout_1 = __importDefault(__webpack_require__(/*! ../../../components/common/layout */ "./resources/js/components/common/layout/index.tsx"));
-var BookmarkListPage = function (_a) {
-    var bookmarks = _a.bookmarks;
-    return (react_1.default.createElement(layout_1.default, null,
-        react_1.default.createElement("div", { className: "row" },
-            react_1.default.createElement("div", { className: "col-sm-8" },
-                react_1.default.createElement("ul", { className: "list-group" }, bookmarks.length > 0 &&
-                    bookmarks.map(function (bookmark, index) {
-                        return (react_1.default.createElement("li", { className: "list-group-item", key: index },
-                            react_1.default.createElement("p", null,
-                                "Title:",
-                                bookmark.title)));
-                    }))))));
-};
-exports.default = BookmarkListPage;
-
-
-/***/ }),
-
-/***/ "./resources/js/components/common/layout/index.tsx":
-/*!*********************************************************!*\
-  !*** ./resources/js/components/common/layout/index.tsx ***!
-  \*********************************************************/
+/***/ "./resources/js/Pages/Welcome/index.tsx":
+/*!**********************************************!*\
+  !*** ./resources/js/Pages/Welcome/index.tsx ***!
+  \**********************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -61,58 +28,17 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __importStar(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
-var nav_1 = __importDefault(__webpack_require__(/*! ../nav */ "./resources/js/components/common/nav/index.tsx"));
-var Layout = function (_a) {
-    var children = _a.children;
-    return (React.createElement(React.Fragment, null,
-        React.createElement("div", { className: "mb-3" },
-            React.createElement(nav_1.default, null)),
-        React.createElement("div", { className: "container" }, children),
-        ";"));
+var WelcomePage = function () {
+    return (React.createElement("div", { className: "container" },
+        React.createElement("h1", null, "Welcome Bookmark app"),
+        React.createElement("p", null,
+            React.createElement("a", { href: "/login" }, "Login here")),
+        React.createElement("p", null,
+            React.createElement("a", { href: "/register" }, "Registar here"))));
 };
-exports.default = Layout;
-
-
-/***/ }),
-
-/***/ "./resources/js/components/common/nav/index.tsx":
-/*!******************************************************!*\
-  !*** ./resources/js/components/common/nav/index.tsx ***!
-  \******************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var inertia_react_1 = __webpack_require__(/*! @inertiajs/inertia-react */ "./node_modules/@inertiajs/inertia-react/dist/index.js");
-var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
-var ziggy_js_1 = __importDefault(__webpack_require__(/*! ziggy-js */ "./node_modules/ziggy-js/dist/js/route.min.js"));
-var Menu = function () {
-    return (react_1.default.createElement("nav", { className: "navbar navbar-expand-md navbar-light bg-white shadow-sm" },
-        react_1.default.createElement("div", { className: "container" },
-            react_1.default.createElement("a", { className: "navbar-brand", href: "{{ url('/') }}" }, "Bookmark"),
-            react_1.default.createElement("button", { className: "navbar-toggler", type: "button", "data-toggle": "collapse", "data-target": "#navbarSupportedContent", "aria-controls": "navbarSupportedContent", "aria-expanded": "false", "aria-label": "{{ __('Toggle navigation') }}" },
-                react_1.default.createElement("span", { className: "navbar-toggler-icon" })),
-            react_1.default.createElement("div", { className: "collapse navbar-collapse", id: "navbarSupportedContent" },
-                react_1.default.createElement("ul", { className: "navbar-nav mr-auto" },
-                    react_1.default.createElement("li", { className: "nav-item" },
-                        react_1.default.createElement(inertia_react_1.InertiaLink, { className: "nav-link", href: ziggy_js_1.default('bookmark.index').url() }, "Bookmarks")),
-                    react_1.default.createElement("li", { className: "nav-item" },
-                        react_1.default.createElement(inertia_react_1.InertiaLink, { className: "nav-link", href: ziggy_js_1.default('bookmark.add').url() }, "Bookmark Add"))),
-                react_1.default.createElement("ul", { className: "navbar-nav ml-auto" },
-                    react_1.default.createElement("li", { className: "nav-item" },
-                        react_1.default.createElement("a", { className: "nav-link", href: "#" }, "Logout")))))));
-};
-exports.default = Menu;
+exports.default = WelcomePage;
 
 
 /***/ })
