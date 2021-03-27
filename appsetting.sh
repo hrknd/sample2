fabb64d2ac6a8e143548c1afbd8409fb5f616ac6
KONDO, Hiroyasu <hr.knd@ateria.co.jp>
2021-03-27 03:45:31 +0000

    キャッシュクリア系のコマンドをメモ

--- appsetting.sh
+++ appsetting.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-chown www-data:www-data database/
+chown www-data:www-data database
 chown www-data:www-data database/database.sqlite
 chown www-data:www-data bootstrap/cache
 chmod og+rw database/database.sqlite
+chown -R www-data:www-data storage
 php artisan clean:models
---
+++ cacehclear.sh
@@ -0,0 +1,9 @@
+composer dump-autoload
+php artisan clear-compiled
+php artisan optimize
+php artisan config:cache
+php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
